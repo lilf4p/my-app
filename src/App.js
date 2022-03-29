5eb40d38cdccf055698bc0e0b89719d343e90f84
lilf4p <leostoppa999@gmail.com>
2022-03-29 16:46:32 +0200

    add github api

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './App.css'
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar2 from './components/navbar2';
-import { Card, Container, Row } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import Coding from './components/coding';
 import Study from './components/study';
 import Photos from './components/photos';
 import { Alert } from 'react-bootstrap';
+import axios from 'axios';
 //import Coding from './components/coding';
 
 function App() {
+  const repos = () => axios.get('https://api.github.com/users/lilf4p/repos')
+    .then(function(response) {
+    console.log(response.data)
+  })
   return (
     <>
       <Navbar2 />
@@ -30,6 +35,13 @@ function App() {
           <Coding />
           <Study />
       </Container>
+
+      <Button 
+      
+      onClick={repos}
+      >
+        List Repos
+      </Button>
     </>
   );
 
