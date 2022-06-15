6683ddfa519cd52d435c06485f5e2afc9eb847f3
lilf4p <leostoppa999@gmail.com>
2022-06-15 19:08:30 +0200

    fix

--- src/components/social.js
+++ src/components/social.js
@@ -2,7 +2,8 @@ import { Button, Container } from "react-bootstrap";
 
 function Social () {
     return (
-        <Container id="social" className="Social">
+        <Container fluid="true" id="social" className="Social">
+            <hr />
             <h2>Find me on social media!</h2>
             <Button size="sm" variant="primary" href="https://github.com/lilf4p" target="_blank">Github</Button>{' '}
             <Button size="sm" variant="primary" href="https://www.instagram.com/lilf4p/" target="_blank">Instagram</Button>{' '}
--- src/components/study.js
+++ src/components/study.js
@@ -7,20 +7,8 @@ function Study () {
     return (
         <Card id='study' className="bg-dark text-white">
             <Card.Img src={require("../immagini/laureasfondo.png")} alt="laureasfondo.png" />
-            <Card.ImgOverlay>
-                <Card.Title>Studies</Card.Title>
-                <Card.Text>
-                    Computer Science Degree
-                </Card.Text>
-                <ProgressBar now={now1} label={`${now1}%`} />
-                <Card.Text as="h5">25 Marzo 2022 University of Pisa</Card.Text>
-                <hr/>
-                <Card.Text>
-                    Master of Computer Science Degree
-                </Card.Text>
-                <ProgressBar now={now2} label={`${now2}%`} />
-                <Card.Text as="h5">University of Pisa</Card.Text>
-                <hr/>
+            <Card.ImgOverlay >
+                <Card.Title>Laurea in Informatica</Card.Title>
             </Card.ImgOverlay>
         </Card>
     );
